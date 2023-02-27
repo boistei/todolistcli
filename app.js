59e8452508ce621b5edf78a54f37d61c8c55a108
boistei <29461756+boistei@users.noreply.github.com>
2023-02-27 18:48:57 +0530

    Implement Add, View, Quit

---
+++ app.js
@@ -0,0 +1,27 @@
+let input = prompt("Enter your choice").toLowerCase();
+const list = ["Run roomba", "Approve myGate"];
+
+while(input !== 'quit' || input !== 'q'){
+    if(input === "add"){
+        let addItem = prompt("Enter item");
+        list.push(addItem);
+        console.log(`${addItem} added to the list!`);
+    }
+    else if(input === "view"){
+        console.log("*****************");
+        for(let i = 0; i < list.length; i++)
+            console.log(`${i}: ${list[i]}`);
+        console.log("*****************");
+    }
+    else if(input === "delete"){
+        console.log("delete");
+    }
+    else if(input === "quit"){
+        console.log("quitting");
+        break;
+    }
+    else {
+        input = alert("Invalid choice! Try again");
+    }
+    input = prompt("Enter your choice").toLowerCase();
+}
